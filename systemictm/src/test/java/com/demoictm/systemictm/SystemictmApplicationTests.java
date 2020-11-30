c71691904ea6c2667c0c667fda1053e2975fa64d
Shan | <tdportia225@gmail.com>
2020-11-30 23:52:43 +0530

    first commit

---
+++ systemictm/src/test/java/com/demoictm/systemictm/SystemictmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoictm.systemictm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemictmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
